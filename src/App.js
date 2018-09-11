dfa29eb6db24be25d2ec83062539400462fd8d06
robertmills1985 <robertmills1985@yahoo.com>
2018-09-11 16:22:31 -0700

    refactored shuffle() to eliminate undefined indexes which i cant seem to figure out, but designed basically a bandaid to get me through

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ var dealersHand = [];
 var discardPile = [];
 var playerAce = false;
 var dealerAce = false;
-function aceReducer(a){
+function aceReducer(a) {
 	//in this function have it see if it can run through an array and reduce
 	//aces down to 1 to bring the total value under 21
 }
@@ -58,7 +58,7 @@ function compareScores(playerscore, dealerscore) {
 function pass() {
 	playersTurn = false;
 	var total = addArray(dealersHand);
-	if (total < 16) {
+	if (total < 1) {
 		dealersHand.push(transpose(shuffledDeck[0]));
 		shuffledDeck.shift();
 		console.log(dealersHand, addArray(dealersHand));
@@ -67,8 +67,11 @@ function pass() {
 		alert(' Dealer Busts You Win');
 		yourHand = [];
 		dealersHand = [];
+		console.log(dealersHand, addArray(dealersHand));
+
 		startGame();
 	} else {
+
 		compareScores(addArray(yourHand), addArray(dealersHand));
 	}
 }
@@ -119,8 +122,8 @@ function transpose(a) {
 }
 //startGame is what starts the game by dealing to players
 function startGame() {
-	if(shuffledDeck.length < 35){
-		shuffle()
+	if (shuffledDeck.length < 35) {
+		shuffle();
 	}
 	for (var i = 1; i < 5; i++) {
 		if (i % 2 === 0) {
@@ -161,67 +164,85 @@ function hitMe(a) {
 }
 
 //generates a deck
-for (var i = 0; i <= suitCount; i++) {
-	if (i == 1) {
-		deck.push('Ace of Spades');
-	} else if (i > 1 && i <= 10) {
-		deck.push(i + ' of Spades');
-	} else if (i == 11) {
-		deck.push('Jack of Spades');
-	} else if (i == 12) {
-		deck.push('Queen of Spades');
-	} else if (i == 13) {
-		deck.push('King of Spades');
+function generateDeck() {
+	for (var i = 0; i <= suitCount; i++) {
+		if (i == 1) {
+			deck.push('Ace of Spades');
+		} else if (i > 1 && i <= 10) {
+			deck.push(i + ' of Spades');
+		} else if (i == 11) {
+			deck.push('Jack of Spades');
+		} else if (i == 12) {
+			deck.push('Queen of Spades');
+		} else if (i == 13) {
+			deck.push('King of Spades');
+		}
+	}
+	for (var i = 0; i <= suitCount; i++) {
+		if (i == 1) {
+			deck.push('Ace of Hearts');
+		} else if (i > 1 && i <= 10) {
+			deck.push(i + ' of Hearts');
+		} else if (i == 11) {
+			deck.push('Jack of Hearts');
+		} else if (i == 12) {
+			deck.push('Queen of Hearts');
+		} else if (i == 13) {
+			deck.push('King of Hearts');
+		}
+	}
+	for (var i = 0; i <= suitCount; i++) {
+		if (i == 1) {
+			deck.push('Ace of Diamonds');
+		} else if (i > 1 && i <= 10) {
+			deck.push(i + ' of Diamonds');
+		} else if (i == 11) {
+			deck.push('Jack of Diamonds');
+		} else if (i == 12) {
+			deck.push('Queen of Diamonds');
+		} else if (i == 13) {
+			deck.push('King of Diamonds');
+		}
+	}
+	for (var i = 0; i <= suitCount; i++) {
+		if (i == 1) {
+			deck.push('Ace of Clubs');
+		} else if (i > 1 && i <= 10) {
+			deck.push(i + ' of Clubs');
+		} else if (i == 11) {
+			deck.push('Jack of Clubs');
+		} else if (i == 12) {
+			deck.push('Queen of Clubs');
+		} else if (i == 13) {
+			deck.push('King of Clubs');
+		}
 	}
 }
-for (var i = 0; i <= suitCount; i++) {
-	if (i == 1) {
-		deck.push('Ace of Hearts');
-	} else if (i > 1 && i <= 10) {
-		deck.push(i + ' of Hearts');
-	} else if (i == 11) {
-		deck.push('Jack of Hearts');
-	} else if (i == 12) {
-		deck.push('Queen of Hearts');
-	} else if (i == 13) {
-		deck.push('King of Hearts');
-	}
-}
-for (var i = 0; i <= suitCount; i++) {
-	if (i == 1) {
-		deck.push('Ace of Diamonds');
-	} else if (i > 1 && i <= 10) {
-		deck.push(i + ' of Diamonds');
-	} else if (i == 11) {
-		deck.push('Jack of Diamonds');
-	} else if (i == 12) {
-		deck.push('Queen of Diamonds');
-	} else if (i == 13) {
-		deck.push('King of Diamonds');
-	}
-}
-for (var i = 0; i <= suitCount; i++) {
-	if (i == 1) {
-		deck.push('Ace of Clubs');
-	} else if (i > 1 && i <= 10) {
-		deck.push(i + ' of Clubs');
-	} else if (i == 11) {
-		deck.push('Jack of Clubs');
-	} else if (i == 12) {
-		deck.push('Queen of Clubs');
-	} else if (i == 13) {
-		deck.push('King of Clubs');
-	}
-}
+
 //shuffles the deck in the beginning
 function shuffle() {
+	console.log(
+		'Deck is being reshuffled'
+	)
 	shuffledDeck = []
+	var totalUndefined = 0;
 	for (var x = 0; x < 52; x++) {
 		var random = Math.floor(Math.random() * deck.length + 1);
 		shuffledDeck.push(deck[random]);
 	}
-	console.log("Deck has been re shuffled.")
+	for (var i = 0; i < shuffledDeck.length; i++) {
+		if (shuffledDeck[i] === undefined) {
+			totalUndefined = totalUndefined + 1;
+			shuffledDeck[i] = '2 of Hearts'
+		}
+	}
+	if (totalUndefined > 0) {
+		console.log(totalUndefined)
+	}
+	console.log(shuffledDeck)
+	
 }
+generateDeck();
 shuffle()
 
 class App extends Component {
