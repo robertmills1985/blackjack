b7460af7d6c8fe25e360d3072065ca605da0b84f
robertmills1985 <robertmills1985@yahoo.com>
2018-10-01 23:07:01 -0700

    built a card component, but really not there on the css. funtionality is almost there but im confused on how to rerender after a card has been added to display the current hand. i might have to totally over haul it to be all state functions

---
+++ src/Card.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import './App.css'
+
+//testing variables
+var d1 = [ 'Card 1', 'Card 2' ];
+
+//function scanHand(hand) {
+//	var thisHand = [];
+//	for (var i = 0; i < hand.length; i++) {
+//		thisHand.push(<li>{hand[i]}</li>);
+//	}
+//	return thisHand;
+//}
+
+class Card extends Component {
+	constructor() {
+        super();
+	}
+	render() {
+		function scanHand(inHand) {
+			var thisHand = [];
+			for (var i = 0; i < inHand.length; i++) {
+                thisHand.push(<li>{inHand[i]}</li>);
+                console.log(inHand)
+                console.log(inHand[i])
+			}
+			return thisHand;
+		}
+        var hand = scanHand(this.props.hand);
+		return (
+			<div class=''>
+				<div class='z-depth-5 hand-panel'>
+					<ul class='center'>{hand}</ul>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Card;
